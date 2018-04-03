c01753e3ee51651ec3ac51be2470ea0de080351e
bsasscer <smithbi.maricela@gmail.com>
2018-04-03 12:01:13 -0700

    remove dummy data, connect with ContactsAPI, use remove method so when a contact is deleted, the database is updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
 
     state = {
-        contacts : [
-            {
-                "id": "karen",
-                "name": "Karen Isgrigg",
-                "handle": "karen_isgrigg",
-                "avatarURL": "http://localhost:5001/city.jpg"
-            },
-            {
-                "id": "richard",
-                "name": "Richard Kalehoff",
-                "handle": "richardkalehoff",
-                "avatarURL": "http://localhost:5001/trees.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "handle": "tylermcginnis",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts : []
+    }
+
+    componentDidMount() {
+        ContactsAPI.getAll()
+        .then((contacts) => {
+            this.setState(() => ({
+                contacts
+            }))
+        })
     }
 
     removeContact = (contact) => {
@@ -33,7 +24,11 @@ class App extends Component {
                 return c.id !== contact.id
             })
         }))
+
+        ContactsAPI.remove(contact)
     }
+
+
 
     render() {
         return (
